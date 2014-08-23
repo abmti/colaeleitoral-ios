e14f22783248fb8a18e4d8b9250006d9a15b657a
Adriano <byadriano@gmail.com>
2014-08-23 09:51:39 -0300

    Initial commit

---
+++ ColaEleitoral/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColaEleitoral
+//
+//  Created by Adriano Medeiros on 23/08/14.
+//  Copyright (c) 2014 Adriano Medeiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
